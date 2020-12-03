996b1ad1d1488d37e035ba07b118dfb6c80caa29
Nico Bazzoni <you@example.com>
2020-12-03 18:14:30 -0500

    commit tree 2430d04804467075a42f77cef36c39639df55502

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,53 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    song = self.new
+    self.all << song
+    song
+  end
 
-end
+  def self.new_by_name(title)
+    song = self.new
+    song.name = title 
+    song
+  end
+
+  def self.create_by_name(title)
+    song = self.create
+    song.name = title
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find{|x| x.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.alphabetical 
+    @@all.sort_by{|i|i.name}
+  end
+  def self.new_from_filename(filename)
+    song_array = filename.split(" - ")
+    song_array[1] = song_array[1].chomp(".mp3")
+    song = self.new
+    song.name = song_array[1]
+    song.artist_name = song_array[0]
+    song
+  end
+    def self.create_from_filename(filename)
+      result = self.new_from_filename(filename)
+      song = self.create
+      song.name = result.name
+      song.artist_name = result.artist_name
+      song
+    end
+    
+    def self.destroy_all
+      self.all.clear
+    end
+  end
